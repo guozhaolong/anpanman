9e3805e8cc19b3802ea5e7f3f3fa035029afd191
fjl716 <fjl716@163.com>
2019-01-02 22:31:39 +0800

    增加sqlite测试代码

--- models/asset.js
+++ models/asset.js
@@ -1,5 +1,8 @@
 import createModel from './index'
 import {asset} from '../services/simulator';
+import Expo, {SQLite} from 'expo';
+
+const db = SQLite.openDatabase('db.db');
 
 export default createModel('asset', {
 
@@ -22,7 +25,18 @@ export default createModel('asset', {
         }
       });
       const data = yield call(asset, q);
-      if (data) yield put({ type: 'setValue', payload: { items: data.list, total: data.total } })
+      db.transaction(tx => {
+        tx.executeSql(
+          'create table if not exists items (id integer primary key not null, done int, value text);'
+        );
+      });
+      db.transaction(tx => {
+        tx.executeSql('insert into items (done, value) values (0, ?)', ['test']);
+        tx.executeSql('select * from items', [], (_, { rows }) =>
+          console.log(rows._array)
+        );
+      });
+      if (data) yield put({type: 'setValue', payload: {items: data.list, total: data.total}})
     },
   },
   reducers: {
