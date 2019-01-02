daa99a8afe2eb30b019cfe0cee17eafe4d9741e6
fjl716 <fjl716@163.com>
2019-01-02 22:21:44 +0800

    修改bug

--- models/asset.js
+++ models/asset.js
@@ -1,27 +1,27 @@
 import createModel from './index'
-import {asset} from "../services/simulator";
+import {asset} from '../services/simulator';
 
 export default createModel('asset', {
 
   state: {},
 
   subscriptions: {
-    async setup({ dispatch }) {
+    async setup({dispatch}) {
 
     },
   },
   effects: {
-    * fetch({ payload }, { select, call, put }) {
+    * fetch({payload}, {select, call, put}) {
       const q = yield select((state) => {
-        const { pageSize, currentPage, query, orderBy } = state
+        const {pageSize, currentPage, query, orderBy} = state
         return {
           pageSize,
           currentPage,
           query,
           orderBy,
         }
-      })
-      const data = yield call(asset, name, q)
+      });
+      const data = yield call(asset, q);
       if (data) yield put({ type: 'setValue', payload: { items: data.list, total: data.total } })
     },
   },
